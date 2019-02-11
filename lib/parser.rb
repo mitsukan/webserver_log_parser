dd9c5939f06ed6c83858d7ffb0b615682744ec57
Jihin Ip <jim_ip20@hotmail.com>
2019-02-11 17:04:09 +0000

    Test for the method parse to insert page and IP address

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,6 +1,16 @@
-class Parse
+class Parser
   attr_reader :webpages
   def initialize
     @webpages = {}
+  end
+
+  def parse(filename)
+    lines = File.readlines filename
+    for line in lines
+      ip_address = line.scan(/\s.*\z/)[0]
+      page = line.sub(ip_address, '')
+      @webpages[page] = ip_address.strip
+    end
+    @webpages
   end
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,8 +1,16 @@
 require 'parser'
 
-describe Parse do
+describe 'Parser' do
   it 'Adds the webpages to a hash' do
-    parser = Parse.new
+    parser = Parser.new
     expect(parser.webpages).to eq({})
+  end
+
+  describe '.parse' do
+    it 'Parses a log file' do
+      parser = Parser.new
+      parser.parse('./spec/test.log')
+      expect(parser.webpages).to eq({'/help_page/1' => '126.318.035.038'})
+    end
   end
 end
